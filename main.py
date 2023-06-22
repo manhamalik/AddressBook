124ae9f4558a26b5054e525a1bad83d79edb78d8
mxnha77 <manhamalik77@gmail.com>
2023-06-22 08:43:02 +0000

    Your commit message

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from dataclasses import dataclass
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Data class representing a contact
+@dataclass
+class Contact:
+    name: str
+    address: str
+    phone: str
+
+# A list to store the contacts
+contacts = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'delete' in request.form:
+            # Get the index of the contact to delete
+            contact_index = int(request.form['delete'])
+            if 0 <= contact_index < len(contacts):
+                del contacts[contact_index]
+        else:
+            # Get the form data
+            name = request.form.get('name')
+            address = request.form.get('address')
+            phone = request.form.get('phone')
+
+            # Create a new Contact instance
+            contact = Contact(name=name, address=address, phone=phone)
+
+            # Add the contact to the address book
+            contacts.append(contact)
+
+    return render_template('index.html', contacts=contacts)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81)
